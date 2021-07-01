971d4cb6a69ddb8821de70736950bd07ee7067f5
Pol Reig <polreigbroto@gmail.com>
2021-07-01 18:07:20 +0200

    Archivos y carpetas de la carpeta usuario

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
+const { username, homedir } = os.userInfo();
+
+const bytesToMb = (bytes) => (bytes / 1024 / 1024).toFixed(2);
 
 console.log("Hackeando tu sistema operativo...");
-
 setTimeout(() => {
   if (process.platform === "win32") {
     console.log("Tú no molas mucho");
@@ -12,12 +17,28 @@ setTimeout(() => {
   } else if (process.platform === "linux") {
     console.log("Tú molas");
   }
+
+  console.log(`Cuidado, te quedan ${bytesToMb(os.freemem())} Mb de RAM libre`);
+  console.log(`La versión de tu sistema operativo es ${os.version()}`);
+  console.log(
+    `Tu usuario del sistema operativo es ${username} y tu carpeta es ${homedir}`
+  );
+  console.log("Éstos son los archivos y carpetas de tu carpeta de usuario: ");
+  fs.readdir(homedir, (err, archivos) => {
+    if (err) {
+      console.log(`¡Error, algo ha petado! ${err.message}`);
+      return;
+    }
+    for (const archivo of archivos) {
+      fs.stat(path.join(homedir, archivo), (err, info) => {
+        console.log(
+          "\n",
+          info.isDirectory()
+            ? `${archivo} es un directorio que ocupa ${info.size} Bytes`
+            : `${archivo} es un archivo que ocupa ${info.size} Bytes`
+        );
+        console.log("\n#############################");
+      });
+    }
+  });
 }, 2000);
-
-const os = require("os");
-
-console.log(`Cuidado, te quedan ${os.freemem()} Mb de RAM libre`);
-console.log(`La versión de tu sistema operativo es ${os.version()}`);
-console.log(
-  `Tu usuario del sistema operativo es ${os.homedir()} y tu carpeta es ${__dirname}`
-);
